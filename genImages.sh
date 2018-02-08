5cafd3b4a271cdf27f969b0a24e349b88cdd7102
wecomeon <1171569161liu@sina.com>
2018-02-08 14:02:15 +0800

    Update genImages.sh

--- genImages.sh
+++ genImages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DMALL_DOCKER_REGISTRY='ec2-54-95-48-23.ap-northeast-1.compute.amazonaws.com:5000'
-SLUG='dmall'
+SLUG='liuchenwei'
 
 sudo docker build -t $DMALL_DOCKER_REGISTRY/$SLUG/inventory-service:$BUILD_NUMBER .
 sudo docker tag $DMALL_DOCKER_REGISTRY/$SLUG/inventory-service:$BUILD_NUMBER $DMALL_DOCKER_REGISTRY/$SLUG/inventory-service:latest
